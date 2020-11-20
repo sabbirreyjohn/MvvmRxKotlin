1d39a0e3116dc42fadb4b58da22e04e3b8deb34f
Reyjohn <sabbir.ahmed.khan.reyjohn@gmail.com>
2020-11-20 11:52:59 +0600

    Initial Commit

---
+++ app/src/main/java/com/androidrey/mvvmrxkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androidrey.mvvmrxkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
